c991552967d5dfd7bfeb993df7c05242aaca283b
gaurav251 <39879224+gaurav251@users.noreply.github.com>
2022-04-01 08:02:45 +0530

    Time: 36 ms (83.18%), Space: 13.8 MB (63.41%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,8 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        string_digits = ""
-        for value in digits:
-            string_digits += str(value)
-        int_digits = int(string_digits) + 1
-        string_digits = str(int_digits)
-        return [int(i) for i in string_digits]
+        s=''
+        for i in digits:
+            
+            s+=str(i)
+            
+        s = int(s)+1
+        s = str(s)
+        
+        return [int(i) for i in s]
